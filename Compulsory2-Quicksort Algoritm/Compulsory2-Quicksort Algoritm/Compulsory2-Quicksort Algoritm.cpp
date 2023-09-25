f4929af2db712da955a750e0aaed982e27cc8328
CathrineSageng <111863594+CathrineSageng@users.noreply.github.com>
2023-09-25 20:19:57 +0200

    Quicksort done

--- Compulsory2-Quicksort Algoritm/Compulsory2-Quicksort Algoritm/Compulsory2-Quicksort Algoritm.cpp
+++ Compulsory2-Quicksort Algoritm/Compulsory2-Quicksort Algoritm/Compulsory2-Quicksort Algoritm.cpp
@@ -6,42 +6,47 @@
 using namespace std;
 
 /// <summary>
-/// 
+/// This function is partitioning the array. It splits the array to sub arrays where you have numbers less than the pivot 
+/// and greater than the pivot. Chose th pivot to be the last index of the array.
 /// </summary>
-/// <param name="quickSortArray"></param>
-/// <param name="lowNumber"></param>
-/// <param name="highNumber"></param>
-/// <returns></returns>
-int quickSortPartition(int* quickSortArray, int lowNumber, int highNumber)
+/// <param name="quickSortArray"> The integers in the array to be sorted </param>
+/// <param name="mostLeftIndex"> This number is used to define the first index in the array </param>
+/// <param name="mostRightIndex"> This number is used ro define the last number in the array </param>
+/// <returns> returns the pivotIndex after the last index is swapped</returns>
+int quickSortPartition(int* quickSortArray, int mostLeftIndex, int mostRightIndex) 
 {
-	int pivot = quickSortArray[highNumber];
-	int i = lowNumber - 1;
+	int pivot = quickSortArray[mostRightIndex];
+	int i = mostLeftIndex - 1;
 
-	for (int j = lowNumber; j <= highNumber - 1; j++)
+	for (int j = mostLeftIndex; j <= mostRightIndex - 1; j++)  
 	{
-		if (quickSortArray[j] < pivot)
+		if (quickSortArray[j] < pivot)	
 		{
-			i++;
+			i++;					
 			swap(quickSortArray[i], quickSortArray[j]);
 		}
 	}
-	swap(quickSortArray[i + 1], quickSortArray[highNumber]);
-	return i + 1;
+	swap(quickSortArray[i + 1], quickSortArray[mostRightIndex]);
+	return i + 1; 
 }
 
 /// <summary>
-/// 
+/// This function calls itself with recursion. Uses a divide and conquer to divide the array to smaller pieces for sorting. 
+/// The pivot tells us where the split is between the left part and the right part of the array.
+/// The first recusion sorts the left part of the array uptil the pivot, but does not include the pivot. 
+/// The second recursion sorts the right part of the array after the pivot, but does not include the pivot. 
+/// The base case is when the most left index is not samller than the right most index. 
 /// </summary>
-/// <param name="quickSortArray"></param>
-/// <param name="lowNumber"></param>
-/// <param name="highNumber"></param>
-void recursionQuickSort(int* quickSortArray, int lowNumber, int highNumber)
+/// <param name="quickSortArray"> The integers in the array to be sorted </param>
+/// <param name="lowNumber"> This number is used to define the first index in the array </param>
+/// <param name="highNumber"> This number is used ro define the last number in the array </param>
+void recursionQuickSort(int* quickSortArray, int mostLeftIndex, int mostRightIndex)
 {
-	if (lowNumber < highNumber)
+	if (mostLeftIndex < mostRightIndex) 
 	{
-		int pivotIndex = quickSortPartition(quickSortArray, lowNumber, highNumber);
-		recursionQuickSort(quickSortArray, lowNumber, pivotIndex - 1);
-		recursionQuickSort(quickSortArray, pivotIndex + 1, highNumber);
+		int pivotIndex = quickSortPartition(quickSortArray, mostLeftIndex, mostRightIndex);  
+		recursionQuickSort(quickSortArray, mostLeftIndex, pivotIndex - 1); 
+		recursionQuickSort(quickSortArray, pivotIndex + 1, mostRightIndex); 
 	}
 }
 
@@ -67,8 +72,7 @@ void printsOutNumbersOfTheArrayBeforeSorted(int* quickSortArray, int sizeOfTheAr
 			cout << quickSortArray[i] << " ";
 		}
 	}
-	cout << endl;
-	cout << endl;
+	cout << endl<< endl;
 }
 
 /// <summary>
@@ -88,8 +92,7 @@ void printsOutNumbersOfTheArrayAfterSorted(int* quickSortArray, int sizeOfTheArr
 		}
 		cout << quickSortArray[i] << " ";
 	}
-	cout << endl;
-	cout << endl;
+	cout << endl<< endl;
 }
 
 int main()
